924a3aaf7413b2dde8559e5f45e1969320f76362
Chibudom Chikwendu <chibudomchikwendu@gmail.com>
2023-05-10 13:01:21 +0100

    MODIFIED

--- index.js
+++ index.js
@@ -29,9 +29,23 @@ const url = require('url');
 
 // 
 // SERVER
-const tempOverview = fs.readFileSync(`${__dirname}/templates/templates-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/templates-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/templates-product.html`, 'utf-8');
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+    return output
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
@@ -42,7 +56,10 @@ const server = http.createServer((req, res) => {
     // Overview Page
     if (pathName === '/' || pathName === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' });
-        res.end(tempOverview)
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        res.end(output)
 
         // Product Page
     } else if (pathName === '/product') {
