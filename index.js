bdd7bc8d564c932087b3d481d79d704d1a300629
Chibudom Chikwendu <chibudomchikwendu@gmail.com>
2023-05-10 09:17:00 +0100

    Commit Now

---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const http = require('http');
+
+// // const hello = 'Hello World';
+// // console.log(hello);
+
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn)
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('File Written!');
+
+// Non-blocking asynchronous key
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been written😃');
+//             })
+//         });
+//     });
+// });
+// console.log('Will read File');
+
+// 
+// SERVER
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to request on port: 8000');
+})
